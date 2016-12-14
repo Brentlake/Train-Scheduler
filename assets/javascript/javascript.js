4eb3e86765b74e4483e145b182a36c3b6929a529
Brenton Lake <Brenton Lake>
2016-12-14 17:29:05 -0600

    almost done

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -28,10 +28,41 @@
           role: role,
           start: start,
           monthly: monthly
-      });
+  
+});
+      //how often the train runs
+  var tFrequency = monthly;
+
+      // Start Time
+      var firstTime = start;
 
 
-  });
+
+      // First Time (pushed back 1 year to make sure it comes before current time)
+      var firstTimeConverted = moment(firstTime, "hh:mm");
+      console.log(firstTimeConverted);
+
+      // Current Time
+      var currentTime = moment();
+      console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+      // Difference between the times
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      console.log("DIFFERENCE IN TIME: " + diffTime);
+
+      // Time apart (remainder)
+      var tRemainder = diffTime % tFrequency;
+      console.log(tRemainder);
+
+      // Minute Until Train
+      var tMinutesTillTrain = tFrequency - tRemainder;
+      console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+      // Next Train
+      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+      console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+      var arrivalTime = moment(nextTrain).format("hh:mm");
 
   database.ref().on("child_added", function(snapshot) {
       // Log everything that's coming out of snapshot
@@ -42,11 +73,14 @@
       var start = data.start;
       var monthly = data.monthly;
       var trTag = $("<tr>");
+      
 
-      trTag.append("<td>" + name + "</td>" + "<td>" + role + "</td>" + "<td>" + start + "</td>" + "<td>" + monthly + "</td>");
+      trTag.append("<td>" + name + "</td>" + "<td>" + start + "</td>" + "<td>" + monthly + "</td>" + "<td>" + arrivalTime + "</td>" + "<td>" + tMinutesTillTrain + "</td>" + "<td>");
       $("#tbody").append(trTag);
 
       // Handle the errors
   }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
   });
+});
+    
